e2e66be4a3e58c1cff815b7646e0c127af9978ff
kanji1337 <72388637+kanji1337@users.noreply.github.com>
2023-06-08 08:27:18 +0300

    bred nomer2

---
+++ PhoneWord/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace PhoneWord
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            EditText phoneNumberText = (EditText)FindViewById(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = (TextView)FindViewById(Resource.Id.TranslatedPhoneword);
+            Button translateButton = (Button)FindViewById(Resource.Id.TranslateButton);
+
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber =
+             Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
